1b45b7be2cbfa335896d7a0d056dd8c07c64d554
Felix Palmer <felixpalmer@gmail.com>
2014-07-12 13:58:25 +0200

    Add shots with normalization

--- scratchpad/script.js
+++ scratchpad/script.js
@@ -3,6 +3,9 @@
 
 // Game state is contiained in this structure, which is synchronized between devices
 var gameState,
+    me,
+    opponent,
+    fire,
     render;
 
 // Create SVG to hold game arena
@@ -26,7 +29,10 @@ var drag = d3.behavior.drag()
   .origin(function(d) { return d.position; })
   .on('dragstart', function( d ) {
     // Stash away drag start location
-    d._dragStart = d3.event.sourceEvent;
+    d._dragStart = {
+      x: d3.event.sourceEvent.x,
+      y: d3.event.sourceEvent.y
+    };
   })
   .on('dragend', function( d ) {
     var dragDirection = {
@@ -35,27 +41,56 @@ var drag = d3.behavior.drag()
     };
     console.log( 'Dragged in direction:' );
     console.log( dragDirection );
-    render();
+    var shot = {
+      position: d._dragStart,
+      direction: dragDirection,
+      player: me
+    };
+
+    fire( shot );
   });
+
+var fire = function( shot ) {
+  // Normalize direction
+  var factor = shot.direction.y > 0 ? ( height - shot.position.y ) / shot.direction.y : shot.position.y / shot.direction.y;
+  shot.direction.x *= factor;
+  shot.direction.y *= factor;
+  gameState.shots.push( shot );
+  render();
+};
 
 // Rendering, binds actions to SVG elements using d3
 var render = function() {
   // Add creatures for player one (assuming it's `me`)
   var creatures = arena.selectAll( 'circle.me' )
-    .data( gameState.players[1].creatures ) // Bind creatures data set to selection
+    .data( me.creatures ) // Bind creatures data set to selection
     .enter().append( 'circle' )
-    .call( positionCreature )
-    .classed('me', true);
+      .classed('me', true)
+      .call( positionCreature );
   
   // Add creatures for player two (assuming it's the opponent)
-  var opponents = arena.selectAll( 'circle.opponent' )
-    .data( gameState.players[0].creatures ) // Bind creatures data set to selection
+  arena.selectAll( 'circle.opponent' )
+    .data( opponent.creatures ) // Bind creatures data set to selection
     .enter().append( 'circle' )
-    .call( positionCreature )
-    .classed('opponent', true);
+      .classed('opponent', true)
+      .call( positionCreature );
 
   // Bind drag behaviour to creatures
   creatures.call( drag );
+
+  // Add shots
+  arena.selectAll( 'circle.shot' )
+    .data( gameState.shots )
+    .enter().append( 'circle' )
+      .classed('shot', true)
+      .attr('r', 10)
+      .attr('cx', function(d) { return d.position.x; })
+      .attr('cy', function(d) { return d.position.y; })
+      .style('fill', '#ffffff')
+    .transition()
+      .ease( 'linear' )
+      .attr('cx', function(d) { return d.position.x + d.direction.x; })
+      .attr('cy', function(d) { return d.position.y + d.direction.y; });
 };
 
 d3.json('data.json', function(error, json) {
@@ -63,5 +98,8 @@ d3.json('data.json', function(error, json) {
     return console.warn(error);
   }
   gameState = json;
+  // TODO do not hardcode
+  me = gameState.players[0];
+  opponent = gameState.players[1];
   render();
 });
