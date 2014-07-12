e9336bf1f28ef4acee670a4c37944b193d6a3fa1
Daniel Chabr <danielchabr@gmail.com>
2014-07-12 17:49:36 +0200

    Socket test

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -4,6 +4,12 @@ angular.module('mean.system')
 .controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 }])
-.controller('CreatureController', ['$scope', 'Global', function ($scope, Global) {
+.controller('CreatureController', ['$scope', 'Global', 'socket', function ($scope, Global, socket) {
+	socket.on('hello', function () {
+		console.log('hello');
+	});
+	socket.on('broad', function () {
+		console.log('broadcast');
+	});
     $scope.global = Global;
 }]);
