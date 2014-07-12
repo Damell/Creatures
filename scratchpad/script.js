b70e9ecdc032c00ff14d463937d8a44c7e49ddd8
Felix Palmer <felixpalmer@gmail.com>
2014-07-12 13:11:30 +0200

    Position creatures on field

--- scratchpad/script.js
+++ scratchpad/script.js
@@ -1,8 +1,40 @@
 /*global d3*/
+'use strict';
 console.log( 'hi' );
-d3.json("data.json", function(error, json) {
+var render = function( data ) {
+  // Create SVG to hold game arena
+  var width = 800;
+  var height = 600;
+  var arena = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g');
+
+  var positionCreature = function( creature ) {
+    creature
+      .attr('cx', function(d) { return d.position.x; })
+      .attr('cy', function(d) { return d.position.y; })
+      .attr('r', function(d) { return d.health; });
+  };
+
+  // Add creatures for player one (assuming it's `me`)
+  arena.selectAll( 'circle.me' )
+    .data( data.players[0].creatures ) // Bind creatures data set to selection
+    .enter().append( 'circle' )
+    .call( positionCreature )
+    .classed('me', true); 
+  
+  // Add creatures for player two (assuming it's the opponent)
+  arena.selectAll( 'circle.opponent' )
+    .data( data.players[1].creatures ) // Bind creatures data set to selection
+    .enter().append( 'circle' )
+    .call( positionCreature )
+    .classed('opponent', true); 
+};
+
+d3.json('data.json', function(error, json) {
   if (error) {
     return console.warn(error);
   }
-  console.log( json );
+  render( json );
 });
