7d7fbbcc75bc97e02ffa4f7b7ad3609ee06fa763
Daniel Chabr <danielchabr@gmail.com>
2014-07-12 20:01:26 +0200

    Socket test 7

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -6,20 +6,21 @@ angular.module('mean.system')
 }])
 .controller('CreatureController', ['$scope', 'Global', 'socket', function ($scope, Global, socket) {
 	$scope.battleStartUsers = [];
+	var username = window.user.username;
 
-	socket.emit('connect', user.username);
+	socket.emit('connect', username);
 	socket.on('echo', function() {
-		socket.emit('prev_connected', user.username);
+		socket.emit('prev_connected', username);
 	});
 	socket.on('join', function (user) {
 		console.log( 'join ' + user);
 		$scope.battleStartUsers.push(user);
-		$.unique($scope.battleStartUsers);
+		//jQuery.unique($scope.battleStartUsers);
 	});
 	socket.on('prev_connected', function (user) {
 		console.log( 'prev_connected ' + user);
 		$scope.battleStartUsers.push(user);
-		$.unique($scope.battleStartUsers);
+		//jQuery.unique($scope.battleStartUsers);
 	});
 
 	socket.on('join', function () {
