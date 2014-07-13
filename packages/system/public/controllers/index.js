55b3c0a1072a92f3aa9b823a6241b24246e1c955
Daniel Chabr <danielchabr@gmail.com>
2014-07-13 11:51:09 +0200

    --amend

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -50,8 +50,7 @@ angular.module('mean.system')
 	};
 
 	$scope.selectCreature = function (creature) {
-		if ( window.user.battleCreatures.length < 3 
-			&& window.user.battleCreatures.indexOf(creature) === -1 ) {
+		if ( window.user.battleCreatures.length < 3 && window.user.battleCreatures.indexOf(creature) === -1 ) {
 			window.user.battleCreatures.push(creature);
 		}
 	};
